9d77cdf9ea30187813c9be984fb7bf5d409cd6b1
SakethGajawada <77500278+SakethGajawada@users.noreply.github.com>
2022-06-07 20:19:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,39 +53,106 @@ class Flash_Controller(Elaboratable):
         self.jedec_id =                    0b10011111 # 9Fh
         self.read_unique_id =              0b00101011 #4Bh
         
-
+        
+        self.counter =0b0000000000 
+        self.chip_select = 0b1
 
         
 
     def elaborate(self,platform:Platform)->Module:
         m= Module()
-    
-        m.d.comb += self.byte_1.eq(self.data_in[0:7])
-        m.d.sync+= self.byte_1[].eq()
+        m.d.comb += self.chip_select.eq(0b0)
+        m.d.neg += self.byte_1[self.counter].eq(self.data_in[self.counter])
+        m.d.sync +=  self.counter.eq(self.counter + 0b0000000001)
 
 #need to see about chip select
         with m.Switch(self.byte_1):
             with m.Case(self.write_enable):
                 #set write enable latch to 1
-                m.d.comb+=self.status_register[Const(0)].eq(0b1)
+                with m.If(self.counter == 0b0000000100) :
+                    m.d.sync += self.counter.eq(0b0000000000)
+                    m.d.comb += self.chip_select.eq(0b1)
+                    m.d.comb+=self.status_register[Const(0)].eq(0b1)
+            
             with m.Case(self.write_disable):
-                #set write enable latch to 0
-                m.d.comb+=self.status_register[Const(0)].eq(0b0)
-            with m.Case(self.write_enable_VSG):
-                m.d.comb += self.
+                with m.If(self.counter == 0b0000000100) :
+                    m.d.sync += self.counter.eq(0b0000000000)
+                    m.d.comb += self.chip_select.eq(0b1)
+                    m.d.comb+=self.status_register[Const(0)].eq(0b0)
+            # with m.Case(self.write_enable_VSG):
+            #     m.d.comb += self.
+            
             with m.case(self.read_status_reg):
-                with m.If(self.status_register[Const(0)]==0b0):
-                    m.d.comb+=self.data_out[0:7].eq(self.status_register)
-            with m.case(self.read_status_reg):
-                with m.If(self.status_register[Const(0)]==0b0):
-                    m.d.comb+=self.data_out[0:7].eq(self.status_register)
+                with m.If(self.counter == 0b0000000100):
+                    with m.If(self.status_register[Const(0)]==0b0):
+                        m.d.comb+=self.data_out[0:7].eq(self.status_register)
+
             with m.case(self.write_status_reg):
                 with m.If(self.write_protect == 0b1):#write protect pin
                     with m.If(self.status_register[Const(1)]==0b1):
-                        m.d.comb+=self.status_register[Const(7)].eq(self.data_in[Const(7)])
-                        m.d.comb+=self.status_register[Const(5)].eq(self.data_in[Const(5)])
-                        m.d.comb+=self.status_register[Const(4)].eq(self.data_in[Const(4)])
-                        m.d.comb+=self.status_register[Const(3)].eq(self.data_in[Const(3)])
-                        m.d.comb+=self.status_register[Const(2)].eq(self.data_in[Const(2)])
-                        m.d.comb+=self.status_register[Const(0)].eq(0b1)#busy bit  doubt
-                        m.d.comb+=self.status_register[Const(1)].eq(0b0)#wel reset to 0
+                        with m.If(self.counter == 0b0000001000) :
+                            m.d.comb+=self.status_register[Const(7)].eq(self.data_in[Const(7)])
+                            m.d.comb+=self.status_register[Const(5)].eq(self.data_in[Const(5)])
+                            m.d.comb+=self.status_register[Const(4)].eq(self.data_in[Const(4)])
+                            m.d.comb+=self.status_register[Const(3)].eq(self.data_in[Const(3)])
+                            m.d.comb+=self.status_register[Const(2)].eq(self.data_in[Const(2)])
+                            m.d.comb+=self.status_register[Const(0)].eq(0b1)#busy bit  doubt
+                            m.d.comb+=self.status_register[Const(1)].eq(0b0)#wel reset to 0
+                            m.d.comb += self.chip_select.eq(0b1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return m 
+
+    def ports(self)->List[Signal]:
+        return []     
+
+
+if __name__ == "__main__":
+    parser = main_parser()
+    args = parser.parse_args()
+
+    m = Module()
+    m.domains.sync = sync = ClockDomain("sync", async_reset=True)
+    pos = ClockDomain("pos", async_reset=True)
+    neg = ClockDomain("neg",clk_edge="neg",async_reset=True)
+    neg.clk = pos.clk
+    m.domains += [neg,pos]
+
+    m.submodules.Flash_Controller1 = Flash_Controller1 = Flash_Controller()
+
+    sim = Simulator(m)
+    sim.add_clock(1e-6, domain="sync")
+    sim.add_clock(1e-6,domain="pos")
+    sim.add_clock(1e-6,domain = "neg")
+    
+    def process():
+        yield 
+        #yield Delay(1e-6)
+
+
+sim.add_sync_process(process,domain="sync")
+with sim.write_vcd("test.vcd", "test.gtkw", traces=Flash_Controller1.ports()):
+    sim.run_until(100e-6, run_passive=True)
+
+
+
+
+# verilog-------------------------------------------------------------------------------
+# Flash_Controller  = Flash_Controller()
+# frag = Flash_Controller.elaborate(platform=None)
+# print(verilog.convert(frag, ports=Flash_Controller.ports()))
+
